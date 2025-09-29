a3d429902a79025919d6b01803dcab86e92657e4
Bas <bastiansg.bas@gmail.com>
2025-09-29 09:00:26 -0300

    add GlossaryLoader

--- lupai_mw/loaders/__init__.py
+++ lupai_mw/loaders/__init__.py
@@ -2,3 +2,4 @@ from .post_loader import PostLoader  # noqa
 from .file_loader import FileLoader  # noqa
 from .legal_loader import LegalLoader  # noqa
 from .svtipps_loader import SvtippsLoader  # noqa
+from .glossary_loader import GlossaryLoader  # noqa
