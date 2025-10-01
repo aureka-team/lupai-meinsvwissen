9da18c97838d546deb7d88028cb0b7ffdc44bf51
Bas <bastiansg.bas@gmail.com>
2025-10-01 17:55:33 -0300

    improve test_chat script

--- lupai_mw/scripts/chat/test_chat.py
+++ lupai_mw/scripts/chat/test_chat.py
@@ -4,6 +4,8 @@ import time
 import asyncio
 import logfire
 
+from more_itertools import flatten
+
 from rich.panel import Panel
 from rich.prompt import Prompt
 from rich.console import Console
@@ -35,13 +37,6 @@ exit_keywords = {
     "exit",
 }
 
-test_queries = [
-    "What is the difference between consent and compromise?",
-    "How do I organize a student representation?",
-    "Was ist der unterschied zwisschen konsensieren und kompromis?",
-    "Who is Nikola Tesla?",
-    "How can I buy a car?",
-]
 
 germany_regions = [
     "Baden-Württemberg",
@@ -59,7 +54,50 @@ germany_regions = [
     "Sachsen-Anhalt",
     "Schleswig-Holstein",
     "Thüringen",
-    "Landesschülervertretung des Saarlandes",
+    "Landesschülervertretung des Saarlandes",  # TODO: Ask to Jonas.
+]
+
+test_queries = [
+    {
+        "german": "Meine Schüler sind unmotiviert. Was kann ich tun?",
+        "english": "My students are unmotivated. What can I do?",
+    },
+    {
+        "german": "Unsere Schulleitung verbietet uns einen instagram-Account für die SV zu gründen. Ist das legal?",
+        "english": "Our school administration is forbidding us from creating an Instagram account for the student council. Is that legal?",
+    },
+    {
+        "german": "Wie wird die Wahl von Schulsprecher:innen organisiert?",
+        "english": "How is the election of student representatives organized?",
+    },
+    {
+        "german": "Dürfen Klassensprecher:innen von Lehrer:innen abgesetzt werden?",
+        "english": "Are class representatives allowed to be removed by teachers?",
+    },
+    {
+        "german": "Wie bereite ich einen Projekttag für die SV sinnvoll vor? Methoden, Ablaufplan, Vorbereitung",
+        "english": "How do I properly prepare a project day for the student council? Methods, schedule, preparation",
+    },
+    {
+        "german": "Woher kriegen wir Geld für die SV-Arbeit?",
+        "english": "Where can we get money for student council work?",
+    },
+    {
+        "german": "Welche Rechte haben Schüler:innen?",
+        "english": "What rights do students have?",
+    },
+    {
+        "german": "Was gibt es für coole SV-Projekte?",
+        "english": "What are some cool student council projects?",
+    },
+    {
+        "german": "Wir hätten gern ein SV-Seminar an der Schule. Wen können wir dazu anfragen?",
+        "english": "We would like to have a student council seminar at the school. Who can we contact about this?",
+    },
+    {
+        "german": "Nazis sind ein Problem bei uns. Was können wir als SV dagegen tun?",
+        "english": "Nazis are a problem at our school. What can we do as the student council to address this?",
+    },
 ]
 
 
@@ -131,9 +169,12 @@ async def main():
     )
 
     query = None
+    _test_queries = (tq.values() for tq in test_queries)
+    _test_queries = list(flatten(_test_queries))
+
     if option == "1":
         query = option_selection(
-            options=test_queries,
+            options=_test_queries,
             header_message="Select a query:",
             footer_message="Enter the number of the query",
         )
---
+++ lupai_mw/scripts/chat/test_queries.py
@@ -0,0 +1,42 @@
+test_queries = [
+    {
+        "german": "Meine Schüler sind unmotiviert. Was kann ich tun?",
+        "english": "My students are unmotivated. What can I do?",
+    },
+    {
+        "german": "Unsere Schulleitung verbietet uns einen instagram-Account für die SV zu gründen. Ist das legal?",
+        "english": "Our school administration is forbidding us from creating an Instagram account for the student council. Is that legal?",
+    },
+    {
+        "german": "Wie wird die Wahl von Schulsprecher:innen organisiert?",
+        "english": "How is the election of student representatives organized?",
+    },
+    {
+        "german": "Dürfen Klassensprecher:innen von Lehrer:innen abgesetzt werden?",
+        "english": "Are class representatives allowed to be removed by teachers?",
+    },
+    {
+        "german": "Wie bereite ich einen Projekttag für die SV sinnvoll vor? Methoden, Ablaufplan, Vorbereitung",
+        "english": "How do I properly prepare a project day for the student council? Methods, schedule, preparation",
+    },
+    {
+        "german": "Woher kriegen wir Geld für die SV-Arbeit?",
+        "english": "Where can we get money for student council work?",
+    },
+    {
+        "german": "Welche Rechte haben Schüler:innen?",
+        "english": "What rights do students have?",
+    },
+    {
+        "german": "Was gibt es für coole SV-Projekte?",
+        "english": "What are some cool student council projects?",
+    },
+    {
+        "german": "Wir hätten gern ein SV-Seminar an der Schule. Wen können wir dazu anfragen?",
+        "english": "We would like to have a student council seminar at the school. Who can we contact about this?",
+    },
+    {
+        "german": "Nazis sind ein Problem bei uns. Was können wir als SV dagegen tun?",
+        "english": "Nazis are a problem at our school. What can we do as the student council to address this?",
+    },
+]
